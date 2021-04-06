fd9965e4c3b3d8c9a484363acf6d45034f6ce536
xren935 <59517115+xren935@users.noreply.github.com>
2021-04-05 23:15:34 -0700

    header in progress...

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-
+import Button from 'react-bootstrap/Button';
 
 class Header extends React.Component {
   constructor(props) {
@@ -13,7 +13,34 @@ class Header extends React.Component {
   render() {
     return (
         <div className="header">
-            HEADER/HOME
+            <div className="guest-header">
+              <div class="row">
+                <div class="col">
+                  <div className="logo"></div>
+                </div>
+                <div class="col">
+                  <div className="address-search-box">
+                  <i className="fa fa-search fa-2x" aria-hidden="true"></i>
+                </div>
+                <div class="col">
+                  <span>
+                    <Button className="my-button">Log in</Button>
+                    <Button className="my-button">Sign Up</Button>
+                  </span>
+                </div>
+              </div>
+              {/* <div className="logo"></div>
+              <div className="address-search-box">
+                  <i className="fa fa-search fa-2x" aria-hidden="true"></i>
+              </div>
+              {/* <div className="header-btns"> 
+              <span style={{marginLeft: "800px"}}>
+                  <Button>Log in</Button>
+                  <Button>Sign Up</Button>
+              {/* </div> 
+              </span> */}
+              </div>
+            </div>
         </div>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header/Header';
