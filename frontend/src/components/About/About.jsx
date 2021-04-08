c7bbfbe6a7636be646c4002804350ee6f28a9198
YuboC <lalagola@hotmail.com>
2021-04-07 23:42:26 -0400

    About init
    finished Our Mission

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./About.css";
+import { Layout } from 'antd';
+
+const { Content } = Layout;
 
 class About extends React.Component {
   constructor(props) {
@@ -12,7 +15,21 @@ class About extends React.Component {
   render() {
     return (
         <div className="about">
-            ABOUT
+            <Content className="ourMission">
+                <div className="content">
+                    <h1> Our Mission </h1>
+                    <h3>We are an innovative start-up in unmanned delivery.</h3>
+                    <h3>We are making automated drone and robot delivery possible for everyone.</h3>
+                    <span></span>
+                    <h3>Our mission is to improve life and lifestyles by making delivery instant for everyone.</h3>
+                    <h3>Medical delivery, food delivery, ecommerce deliery every day, just in a moment!</h3>
+                </div>
+            </Content>
+            <Content className="ourService">
+              <div className="content">
+                  <h1> Our Service </h1>
+              </div>
+            </Content>
         </div>
     );
   }
