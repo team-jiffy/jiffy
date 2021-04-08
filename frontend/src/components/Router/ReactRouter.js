8a4273d3b723c6a7f5ca18b642b3d2ea7327c0fa
xren935 <59517115+xren935@users.noreply.github.com>
2021-04-08 10:15:42 -0700

    restored footer and added imports in ReactRouter.js

--- frontend/src/components/Router/ReactRouter.js
+++ frontend/src/components/Router/ReactRouter.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from '../../containers/LandingPage/LandingPage';
+import AddressBox from '../../containers/LandingPage/AddressBox';
+import DefaultFooter from '../../containers/DefaultFooter/DefaultFooter';
 import Account from '../../containers/Account/Account';
 import DeliveryInfo from '../../containers/DeliveryInfo/DeliveryInfo';
 import OrderConfirm from '../../containers/OrderConfirm/OrderConfirm';
@@ -8,7 +10,6 @@ import PlaceOrder from '../../containers/PlaceOrder/PlaceOrder';
 import Recommend from '../../containers/Recommend/Recommend';
 import SignPage from '../../containers/SignPage/SignPage';
 import Tracking from '../../containers/Tracking/Tracking';
-
 
 
 class ReactRouter extends Component {
@@ -25,6 +26,9 @@ class ReactRouter extends Component {
                         <Route path="/Recommend" component={Recommend} exact />
                         <Route path="/SignPage" component={SignPage} exact />
                         <Route path="/Tracking" component={Tracking} exact />
+                        <Route path="/LandingPage" component={LandingPage} exact />
+                        <Route path="/AddressBox" component={AddressBox} exact />
+                        <Route path="/DefaultFooter" component={DefaultFooter} exact />
                     </Switch>
              
             </div>
