a12b26d9205063aa24fcb22607930a37f1e69b83
YuboC <lalagola@hotmail.com>
2021-04-08 02:54:55 -0400

    Revert "Merge branch 'frontend' of https://github.com/team-jiffy/jiffy into frontend"
    This reverts commit 8c3c6675d4fa75291019726b91b3e0f201ad62fd, reversing
    changes made to aa99ea350b22ea8210500b0dc21a0bd07967bb91.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header/Header';
 import AddressBox from './components/Header/AddressBox';
 import About from './components/About/About';
-import DefaultFooter from './components/DefaultFooter/DefaultFooter';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <AddressBox />
     <About />
-    <DefaultFooter />
   </React.StrictMode>,
   document.getElementById('root')
 );
