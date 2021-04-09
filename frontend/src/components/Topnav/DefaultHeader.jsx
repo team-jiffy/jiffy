b282810b9130b2c4586e582dcfaf71dceb5addb0
xren935 <59517115+xren935@users.noreply.github.com>
2021-04-08 21:15:55 -0700

    three headers are all set up

---
+++ frontend/src/components/Topnav/DefaultHeader.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./DefaultHeader.css";
+import Button from "react-bootstrap/Button";
+
+class DefaultHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //   loading: true,
+    };
+  }
+
+  render() {
+    return (
+      <div className="default-header">
+        <nav class="navbar">
+          <div class="container-fluid">
+            <div class="row">
+              <a class="navbar-brand" href="#">
+                <div className="logo" alt="JIFFY" />
+              </a>
+              <form>
+                <input
+                  className="address-search-box"
+                  type="search"
+                  placeholder="Enter your tracking number"
+                  aria-label="Search"
+                />
+                <i
+                  className="fa fa-search fa-2x"
+                  style={{ marginLeft: "-40px" }}
+                ></i>
+              </form>
+              <div className="header-buttons">
+                <Button>
+                  <b>Log in</b>
+                </Button>
+                <Button>
+                  <b>Sign up</b>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default DefaultHeader;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactRouter from './components/Router/ReactRouter';
 import { BrowserRouter } from 'react-router-dom';
+// import TrackingNumberHeader from './components/Topnav/TrackingNumberHeader.jsx';
+// import DefaultHeader from './components/Topnav/DefaultHeader.jsx';
+// import DeliveryAddressHeader from './components/Topnav/DefaultHeader.jsx';
 
 ReactDOM.render(
   <BrowserRouter>
