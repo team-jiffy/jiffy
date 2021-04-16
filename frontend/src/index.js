d535dd7f42c3f96c6af4bb0ae1126e9d2599cd92
LinLinLin <77956068+DoreenDoreen@users.noreply.github.com>
2021-04-16 08:39:23 -0500

    SignIn SignUp Components CSS adjustment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,17 @@ import { BrowserRouter } from 'react-router-dom';
 // import LandingPage from './containers/LandingPage/LandingPage';
 // import AddressBox from './containers/LandingPage/AddressBox';
 // import DeliveryAddressHeader from './components/Topnav/DeliveryAddressHeader.jsx';
-import ChangeMyDeliveryAddress from './components/ChangeMyDeliveryAddress/ChangeMyDeliveryAddress.jsx';
+
+// import ChangeMyDeliveryAddress from './components/ChangeMyDeliveryAddress/ChangeMyDeliveryAddress.jsx';
+// import SignUp from './components/SignUp/SignUp';
+// import SignIn from './components/SignIn/SignIn';
 
 ReactDOM.render(
   <BrowserRouter>
     <ReactRouter />
    {/* <ChangeMyDeliveryAddress /> */}
+    {/* <SignUp /> */}
+    {/* <SignIn /> */}
   </BrowserRouter>,
   document.getElementById('root')
 );
