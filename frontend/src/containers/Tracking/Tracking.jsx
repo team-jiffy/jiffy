d520f4ab9078f4ef3c2aa7b32ade4da7d8fa2e44
YuboC <lalagola@hotmail.com>
2021-04-16 15:03:18 -0400

    update Trcking

--- frontend/src/containers/Tracking/Tracking.jsx
+++ frontend/src/containers/Tracking/Tracking.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { Row, Col } from 'antd'
+import DefaultHeader from '../../components/Topnav/DefaultHeader'
+import OrderTrackingProcess from '../../components/OrderTrackingProcess/OrderTrackingProcess'
+import { Layout } from 'antd'
 
-
+const {Content} = Layout;
 class Tracking extends React.Component {
   
   render() {
     return (
-            <div> 
-
-           </div>);
+            <div>
+                <DefaultHeader/>
+                <Row>
+                    <Col span={12}>
+                        <OrderTrackingProcess/>
+                    </Col>
+                    <Col span={12}>
+                        <Content className='right' style={{ padding: '50px 50px', minHeight: 549 }}>
+                            <div className='map'>
+                                MAP
+                            </div>
+                        </Content>
+                    </Col>
+                </Row>
+            </div>);
   }
 }
 
