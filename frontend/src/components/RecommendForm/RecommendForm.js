e42799c80d8baace5dbb2298e5f5c72aea16f2a0
Hayley Huang <73251017+YihanPlus@users.noreply.github.com>
2021-04-19 23:13:18 -0700

    reco form done, except for redirect and css

--- frontend/src/components/RecommendForm/RecommendForm.js
+++ frontend/src/components/RecommendForm/RecommendForm.js
@@ -28,8 +28,7 @@ class RecommendForm extends Component {
                 },
                 weight: null
             },
-            Results: null,
-            ETAList: null,
+            Results: [],
             isLoading: true,
             ChosenReco: {
                 ADVType: null,
@@ -56,8 +55,7 @@ class RecommendForm extends Component {
                 const responseResults = response.data.recos;
                 console.log(responseResults);
                 this.setState({
-                    Results: responseResults,
-                    ETAList: this.getETAList(responseResults),
+                    Results: this.setSettings(responseResults),
                     isLoading: false
                 });
                 console.log(this.state);
@@ -67,39 +65,69 @@ class RecommendForm extends Component {
             });
     }
 
-    getETAList = sourceList => {
-        let ETAList = [];
+    setSettings = sourceList => {
 
-        for (let i = 0; i < sourceList.length; i++) {
-            console.log("sourceList", sourceList[i].eta)
-            let ETA = sourceList[i].eta;
+        const newList = sourceList;
+        console.log("newList", newList);
+
+        let ETAList = [];
+        for (let i = 0; i < newList.length; i++) {
+
+            const object = newList[i];
+
+            const ETA = newList[i].eta;
             console.log("ETA", ETA);
+
+            let list = [];
             if (ETA === null) {
-                return ["same day", "next day"];
+                list = ["same day", "next day"];
+                const key = "ETAList";
+                object[key] = list;
+                newList[i] = object;
+                continue;
             }
+
             // assume stop working hours: 08:00 ~ 20:00
             if (ETA === "20:00") {
-                return ["20:00"];
+                list = ["20:00"];
+                const key = "ETAList";
+                object[key] = list;
+                newList[i] = object;
+                continue;
             }
+
+            list.push(ETA);
+
             let hours = parseInt(ETA.substring(0, 2));
             let mins = parseInt(ETA.substring(3, 5));
             let currMins = hours * 60 + mins;
+            let nextMins;
 
             for (let i = 0; i < 2; i++) {
-                let nextMins = currMins + 30;
+                nextMins = currMins + 30;
                 if (nextMins < 480 || nextMins > 1200) return ETAList;
-                hours = nextMins / 60;
+                hours = Math.floor(nextMins / 60);
                 mins = nextMins % 60;
-                const nextETA = hours + ":" + mins;
-                ETAList = [...ETAList, nextETA];
+                let nextETA;
+                if (mins < 10) {
+                    nextETA = hours + ": 0" + mins;
+                } else {
+                    nextETA = hours + ":" + mins;
+                }
+                list.push(nextETA);
+                currMins = nextMins;
             }
+            const key = "ETAList";
+            object[key] = list;
+            newList[i] = object;
         }
-        return ETAList;
+
+        return newList;
     }
 
-    handleSubmit = e => {
+    clickHandler = (item, e) => {
         e.preventDefault();
-        this.setChosenReco();
+        this.setChosenReco(item);
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
@@ -107,18 +135,20 @@ class RecommendForm extends Component {
             localStorage.setItem("ADVType", this.state.ChosenReco.ADVType);
             localStorage.setItem("Price", this.state.ChosenReco.Price);
             localStorage.setItem("ETA", this.state.ChosenReco.ETA);
-            this.props.history.push('/SignIn');
+            // this.props.history.push('/SignIn');
         })
     }
 
     setChosenReco = (item) => {
+        console.log("item", item);
         this.setState({
             ChosenReco: {
-                ADVType: item.ADVType,
-                Price: item.Price,
-                ETA: item.ETA
+                ADVType: item.advtype,
+                Price: item.price,
+                ETA: item.eta
             }
         });
+        console.log("chosen", this.state.ChosenReco);
     }
 
     render() {
@@ -136,6 +166,16 @@ class RecommendForm extends Component {
 
         function handleChange(value) {
             console.log(`selected ${value}`);
+        }
+
+        function isEqual(object1) {
+            const object2 = {
+                advtype: 'private drone'
+            }
+            const res = object1.advtype === object2.advtype;
+            console.log(object1.advtype);
+            console.log(res);
+            return res;
         }
 
         return (
@@ -156,17 +196,15 @@ class RecommendForm extends Component {
                                 dataSource={this.state.Results}
                                 renderItem={item => (
                                     <List.Item>
-
-                                        <Form className="recommend-form"
-                                              onSubmit={this.handleSubmit}>
+                                        <Form className="recommend-form">
 
                                             <Form.Item {...listItemLayout}>
                                                 <img className="profile" alt="example"
-                                                     src={item.ADVType === 'private drone' ? drone_profile : robot_profile}/>
+                                                     src={isEqual(item) ? drone_profile : robot_profile}/>
                                             </Form.Item>
-
+                                            <br />
                                             <Form.Item {...listItemLayout}>
-                                                <span className="ADVType">{item.ADVType}</span>
+                                                <span className="ADVType">{item.advtype}</span>
                                             </Form.Item>
 
                                             <Form.Item {...formItemLayout} label="Total delivery fee:">
@@ -183,21 +221,22 @@ class RecommendForm extends Component {
                                                         onChange={handleChange}
                                                         showArrow={true}>
                                                     <Option
-                                                        value="default">{this.state.ETAList[0]}</Option>
-                                                    {this.state.ETAList.length > 1 ?
+                                                        value="default">{item.ETAList[0]}</Option>
+                                                    {item.ETAList.length > 1 ?
                                                         <Option
-                                                            value="opt1">{this.state.ETAList[1]}</Option> : console.log('no' +
+                                                            value="opt1">{item.ETAList[1]}</Option> : console.log('no' +
                                                             ' options anymore')}
-                                                    {this.state.ETAList.length > 2 ?
+                                                    {item.ETAList.length > 2 ?
                                                         <Option
-                                                            value="opt2">{this.state.ETAList[2]}</Option> : console.log('no' +
+                                                            value="opt2">{item.ETAList[2]}</Option> : console.log('no' +
                                                             ' options anymore')}
                                                 </Select>
                                             </Form.Item>
 
                                             <Form.Item  {...listItemLayout}>
                                                 <Button className="request-delivery-btn"
-                                                        htmlType="submit">
+                                                        htmlType="submit"
+                                                        onClick={this.clickHandler.bind(this, item)}>
                                                     Request Delivery
                                                 </Button>
                                             </Form.Item>
